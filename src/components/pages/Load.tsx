131d4b409de143e7a797e7e97ec2ffc0e121e179
Catherine <z5359881@student.unsw.edu.au>
2025-03-28 13:42:53 +1100

    load workout not implemented yet

--- src/components/pages/Load.tsx
+++ src/components/pages/Load.tsx
@@ -6,7 +6,7 @@ const Load = (): ReactElement => {
     <>
       <div className="caption">Select:</div>
       <Button type="thick-rect-button" margin="1em auto 0 auto" location="/customs" text="New Workout" />
-      <Button type="thick-rect-button" margin="3em auto 0 auto" location="/load-workouts" text="Load Workout" />
+      <Button type="thick-rect-button" margin="3em auto 0 auto" location="/load" text="Load Workout (Coming soon...)" /> {/* "/load-workouts" */}
     </>
   );
 }
